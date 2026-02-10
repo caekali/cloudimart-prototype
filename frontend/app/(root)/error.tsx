b84f2c974166dc88c833ad04df98af9df04335f7
caesar <ckalikunde@gmail.com>
2026-02-10 12:06:17 +0200

    feat: add ProductError component for error handling and update Layout to include session management

---
+++ frontend/app/(root)/error.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { startTransition, useEffect } from "react";
+
+export default function ProductError({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    console.error("Logged Error:", error);
+  }, [error]);
+
+  const router = useRouter();
+
+  const handleRetry = () => {
+    startTransition(() => {
+      router.refresh();
+      reset();
+    });
+  };
+  return (
+    <div className="p-6 border border-red-200 rounded-lg bg-red-50">
+      <h2 className="text-xl font-semibold text-red-700">
+        Something went wrong!
+      </h2>
+      <p className="text-red-600 mb-4">
+        {error.message || "Failed to load product data."}
+      </p>
+      <button
+        onClick={handleRetry}
+        className="px-4 py-2 bg-red-600 text-white rounded hover:bg-red-700"
+      >
+        Try again
+      </button>
+    </div>
+  );
+}
--- frontend/app/(root)/layout.tsx
+++ frontend/app/(root)/layout.tsx
@@ -1,13 +1,18 @@
+import { auth } from "@/auth";
 import Footer from "@/components/footer";
 import { NavBar } from "@/components/nav-bar";
 import  { ReactNode } from "react";
 
-export default function Layout({ children }: { children: ReactNode }) {
+export default async function Layout({ children }: { children: ReactNode }) {
+  const session = await auth();
   return (
+    
     <div className="min-h-screen bg-gray-50 flex flex-col pb-10">
-      <NavBar />
-      <main className="flex-grow container mx-auto px-4 py-8">{children}</main>
+      <NavBar session={session}/>
+      <main className="flex-grow container mx-auto px-4 sm:px-6 lg:px-8 py-8">{children}</main>
       {/* <Footer /> */}
     </div>
   );
 }
+
+// max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pb-12
