9a44f3066c7f030a7d9a3d0f3c5e86c2fb2b1f69
caesar <ckalikunde@gmail.com>
2026-02-10 12:10:45 +0200

    feat: implement AuthController with email verification, signup, signin, and logout functionalities; add phone column to users table and create personal access tokens migration

---
+++ backend/app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\BaseController;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends BaseController
+{
+    public function verifyEmail(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+        if (! $user) {
+            return $this->errorResponse('Account Not Found', null, 404);
+        }
+
+        return $this->successResponse(status_code: 200);
+
+    }
+
+    public function signup(Request $request)
+    {
+
+        $data = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users,email',
+            'phone' => 'required|string',
+            'password' => 'required',
+        ]);
+
+        $data['password'] = Hash::make($data['password']);
+
+        User::create($data);
+
+         return $this->successResponse(message:'Account Register Successfully',status_code: 201);
+    }
+
+    public function signin(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (Auth::attempt($request->only(['email', 'password']))) {
+            $user = Auth::user();
+
+       
+            return $this->successResponse([
+                'user' => [
+                    "id" => $user->id,
+            "name"=> $user->name,
+            "email" => $user->email,
+                ],
+                'token' => $user->createToken('auth_token')->plainTextToken,
+            ]);
+
+        }
+
+        return $this->errorResponse('Bad credentials', null, 401);
+    }
+
+    public function logout(Request $request)
+    {
+        $user = $request->user();
+        $user->accessToken->delete();
+
+        return response()->noContent();
+    }
+}
